85f2542bee6aa01a522f5cc96e0352f9d7d6aa81
timow-gh <39085531+timow-gh@users.noreply.github.com>
2023-01-22 17:32:59 +0100

    Added CPM and fixed the installation. All targets of the project are installed using one config file. Per component installation is currently not supported.

--- cmake/setup_export_set_installation.cmake
+++ cmake/setup_export_set_installation.cmake
@@ -1,4 +1,4 @@
-function(setup_export_set_installation target_name export_set_name config_cmake_in_file_name major_version minor_version patch_version)
+function(setup_export_set_installation project_name export_set_name config_cmake_in_file_name major_version minor_version patch_version)
     include(GNUInstallDirs)
     include(CMakePackageConfigHelpers)
 
@@ -15,37 +15,37 @@ function(setup_export_set_installation target_name export_set_name config_cmake_
     endif ()
 
     write_basic_package_version_file(
-            ${CMAKE_BINARY_DIR}/${target_name}ConfigVersion.cmake
+            ${CMAKE_BINARY_DIR}/${project_name}ConfigVersion.cmake
             VERSION ${major_version}.${minor_version}.${patch_version}
             COMPATIBILITY SameMajorVersion)
 
     # Name of ${target_name}'s targets file.
-    set(projTargetsFileName "${target_name}Targets")
+    set(projTargetsFileName "${project_name}Targets")
 
     # The cmake module path for ${target_name}.
     set(cmakeModulesDir "${CMAKE_INSTALL_LIBDIR}/cmake")
 
     # Installation path for ${target_name} files.
-    set(cmakeProjDir "${cmakeModulesDir}/${target_name}")
+    set(cmakeProjDir "${cmakeModulesDir}/${project_name}")
 
     # Installation path and file name of ${target_name}'s targets file.
     set(cmakeProjTargetsFilePath "${cmakeProjDir}/${projTargetsFileName}")
 
     configure_package_config_file(
             ${${target_name}_CONFIG_IN_FILE}
-            ${CMAKE_BINARY_DIR}/${target_name}Config.cmake
+            ${CMAKE_BINARY_DIR}/${project_name}Config.cmake
             INSTALL_DESTINATION ${cmakeProjDir}
             PATH_VARS cmakeModulesDir cmakeProjTargetsFilePath
             NO_SET_AND_CHECK_MACRO
             NO_CHECK_REQUIRED_COMPONENTS_MACRO)
 
     install(EXPORT ${export_set_name}
-            NAMESPACE ${target_name}::
+            NAMESPACE ${project_name}::
             FILE ${projTargetsFileName}.cmake
             DESTINATION "${cmakeProjDir}/")
 
     install(FILES
-            "${CMAKE_BINARY_DIR}/${target_name}Config.cmake"
-            "${CMAKE_BINARY_DIR}/${target_name}ConfigVersion.cmake"
+            "${CMAKE_BINARY_DIR}/${project_name}Config.cmake"
+            "${CMAKE_BINARY_DIR}/${project_name}ConfigVersion.cmake"
             DESTINATION "${cmakeProjDir}/")
 endfunction()
