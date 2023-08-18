0ce45fc9279842768573f2e6140252847d128a5a
timow-gh <39085531+timow-gh@users.noreply.github.com>
2023-08-18 21:04:51 +0200

    Make cppcheck optional

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -1,9 +1,9 @@
 include_guard()
 
 function(enable_cppcheck targetName)
-    find_program(CPPCHECK_PROGRAM_PATH cppcheck REQUIRED)
+    find_program(CPPCHECK_PROGRAM_PATH cppcheck)
     if (NOT CPPCHECK_PROGRAM_PATH)
-        message(FATAL_ERROR "cppcheck not found!")
+        message(AUTHOR_WARNING "cppcheck not found!")
     endif ()
     set_target_properties(${targetName}
             PROPERTIES CXX_CPPCHECK
