4bacd96a0fa83913209280276cf62bdcf3a610e4
timow-gh <39085531+timow-gh@users.noreply.github.com>
2024-10-24 01:25:41 +0200

    Use compile_commands.json to execute cppcheck.

--- cmake/cppcheck.cmake
+++ cmake/cppcheck.cmake
@@ -1,6 +1,8 @@
 include_guard()
 
-function(enable_cppcheck targetName)
+# Creates a target cppcheck_target.
+# Requires the compilation db to be present at ${CMAKE_BINARY_DIR}/compile_commands.json
+function(create_cppcheck_target)
     find_program(CPPCHECK_PROGRAM_PATH cppcheck)
 
     if (NOT CPPCHECK_PROGRAM_PATH)
@@ -8,8 +10,12 @@ function(enable_cppcheck targetName)
         return()
     endif ()
 
-    set_target_properties(${targetName}
-            PROPERTIES CXX_CPPCHECK
-            "${CPPCHECK_PROGRAM_PATH};--enable=warning;--suppress=*:*/fmt/*;--error-exitcode=10"
+    set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE BOOL "Export compile commands" FORCE)
+
+    # Make cppcheck_target part of ALL. This ensure it is build along all other targets when building the project.
+    add_custom_target(cppcheck_target ALL
+            COMMAND "${CPPCHECK_PROGRAM_PATH};--project=${CMAKE_BINARY_DIR}/compile_commands.json;--enable=all;--inconclusive;--std=c++${CMAKE_CXX_STANDARD};--suppress=*:*/cpp_proj_template/benchmark/benchmark_example_library.cpp;--suppress=*:*/unixlike-clang-debug/_deps/*;--suppress=*:*/fmt/*;--suppress=*:*/googlemock/*;--suppress=*:*/googletest/*;--suppress=*:*/date-*;--suppress=unmatchedSuppression;--suppress=missingIncludeSystem;--error-exitcode=10"
+            COMMAND_EXPAND_LISTS
+            COMMENT "Running cppcheck on the project"
     )
 endfunction()
