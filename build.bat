7e4da4d0288373e4a23bba80f096c21e767aae4f
timow-gh <39085531+timow-gh@users.noreply.github.com>
2025-05-17 14:30:01 +0200

    Use cmake presets for the build scripts.

--- build.bat
+++ build.bat
@@ -1,43 +1,25 @@
-set REL_REPOSITORY_PATH=.\
-set REPOSITORY_PARENT_PATH=
+@echo off
 
-pushd %REL_REPOSITORY_PATH%
-cd ..\
-set REPOSITORY_PARENT_PATH=%CD%
-popd
-
-echo "REL_REPOSITORY_PATH=%REL_REPOSITORY_PATH%"
-echo "REPOSITORY_PARENT_PATH=%REPOSITORY_PARENT_PATH%"
-
-set SOURCE_DIR=%REPOSITORY_PARENT_PATH%\cpp_proj_template
-echo "SOURCE_DIR=%SOURCE_DIR%"
-
+REM Define preset to use
 set PRESET=windows-msvc-release
+set PACKAGE_PRESET=package-windows
 
-set BUILD_DIR=%REPOSITORY_PARENT_PATH%\cpp_proj_template_build\%PRESET%
-echo "BUILD_DIR=%BUILD_DIR%"
-mkdir %BUILD_DIR%
+REM Configure the project using the preset
+echo Configuring project with preset: %PRESET%
+cmake --preset "%PRESET%"
 
-set INSTALL_DIR=%REPOSITORY_PARENT_PATH%\cpp_proj_template_install\%PRESET%
-echo "INSTALL_DIR=%INSTALL_DIR%"
+REM Build the project
+echo Building project
+cmake --build --preset "%PRESET%" --parallel
 
-cd %SOURCE_DIR%
+REM Run tests
+echo Running tests
+ctest --preset "test-%PRESET%"
 
-cmake   -B %BUILD_DIR% -S %SOURCE_DIR% ^
-        --preset %PRESET% ^
-        -DBUILD_SHARED_LIBS=OFF ^
-        -DCMAKE_CXX_STANDARD=20 ^
-        -Dcpp_proj_template_BENCHMARKS=OFF
+REM Install the project (using the install location from CMakePresets.json)
+echo Installing project
+cmake --install .\out\build\%PRESET%
 
-cmake --build %BUILD_DIR% --parallel 24
-
-pushd %REL_REPOSITORY_PATH%
-
-cd %BUILD_DIR%
-ctest
-cpack
-
-popd
-
-mkdir %INSTALL_DIR%
-cmake --install %BUILD_DIR% --prefix %INSTALL_DIR%
+REM Package the project using the proper package preset command
+echo Creating package
+pack --preset $PACKAGE_PRESET
